18c12d3aac0c34659d300e70302ef1ce967d3253
Daniele <daniele.rb6@gmail.com>
2021-07-25 19:49:30 +0200

    feat: Complete Update EKF function

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -44,7 +44,7 @@ void KalmanFilter::Update(const VectorXd &z) {
   MatrixXd PHt = P_ * Ht;
   MatrixXd K = PHt * Si;
 
-  //new estimate
+  //new state
   x_ = x_ + (K * y);
   long x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
@@ -55,4 +55,35 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float rho = sqrt(px*px + py*py);
+  float theta = atan2(y, x);
+  float ro_dot = (x*vx + y*vy) / rho;
+
+  u = VectorXd(2);
+  u << 0, 0;
+  VectorXd y = z - z_pred;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  Q = MatrixXd(2, 2);
+  Q << 0, 0, 0, 0;
+
+  //new state
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
+  // KF Prediction step
+  x = F * x + u;
+  MatrixXd Ft = F.transpose();
+  P = F * P * Ft + Q;
 }
